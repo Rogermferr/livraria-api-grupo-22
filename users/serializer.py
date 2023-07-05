0c69eaacded301efd741f73844293de1aa7dc94f
Rogermferr <rogermferr@gmail.com>
2023-07-05 19:11:20 -0300

    fix: edit user serializer extra kwargs

--- users/serializer.py
+++ users/serializer.py
@@ -7,16 +7,27 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'cpf', 'password', 'is_superuser', 'full_name', 'block_date', 'is_blocked']
-        extra_kwargs = {'password': {'write_only': True},
-                        'block_date': {'allow_null': True}}
-                        #'is_blocked': {'read_only': True}}
+        fields = [
+            "id",
+            "username",
+            "email",
+            "cpf",
+            "password",
+            "is_superuser",
+            "full_name",
+            "block_date",
+            "is_blocked",
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "block_date": {"allow_null": True, "read_only": True},
+            "is_blocked": {"read_only": True},
+        }
 
     def create(self, validated_data):
-        
-        collaborator = validated_data.get('is_superuser', None)
+        collaborator = validated_data.get("is_superuser", None)
 
         if collaborator:
             return User.objects.create_superuser(**validated_data)
-        
+
         return User.objects.create_user(**validated_data)
