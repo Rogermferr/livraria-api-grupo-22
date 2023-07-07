c7bf3ed0f0f9c31bd9b856a99521462f0e3e674f
MatheusFerreira33 <matheus.fsilva33@gmail.com>
2023-07-07 16:41:59 -0300

    fix:deploy

--- _bookstore/settings.py
+++ _bookstore/settings.py
@@ -59,13 +59,13 @@ INSTALLED_APPS = DJANGO_APPS + MY_APPS + THIRD_PARTY_APPS + ["drf_spectacular"]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "_bookstore.urls"
@@ -100,6 +100,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_NAME"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        
     }
 }
 
--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=127, unique=True)
-    password = models.CharField(max_length=20)
+    password = models.CharField(max_length=200)
     full_name = models.CharField(max_length=255)
     cpf = models.CharField(max_length=11, unique=True)
     is_superuser = models.BooleanField(default=False)
